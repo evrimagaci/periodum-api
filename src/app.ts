8493ab1363e0fa308d8134a3fcecda0daf6fa821
Nesim Tunc <nesimtunc@gmail.com>
2022-02-03 22:01:24 +0900

    Add linters, editorconfig etc.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express from "express";
-const PORT = 3000;
-const app = express();
+import express, { Application, Response } from "express";
 
-app.get("/", (_, res) => {
-  res.status(200).send("Hello Periodum API");
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+app.get("/", (_, res: Response) => {
+	res.status(200).send("Hello Periodum API!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+	// eslint-disable-next-line no-console
+	console.log(`Server running on http://localhost:${PORT}`);
 });
