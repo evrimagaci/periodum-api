4bfcf7efa6360e204e278877ac0cd1cd69c21a44
Tayyib Cankat <tayyib.cankat@adcolony.com>
2022-02-18 03:13:09 +0300

    Change Rate Limiter logic. Use limiter package instead of trying to reinvent the wheel

--- src/pubchem-import/index.ts
+++ src/pubchem-import/index.ts
@@ -1,15 +1,17 @@
-import fetch from "node-fetch";
-import { RateLimiter } from "limiter";
-import { RawCompound } from "./types";
-import getNecessaryData from "./parseData.js";
+import fetch from 'node-fetch';
+import { RateLimiter } from 'limiter';
+import { RawCompound } from './types';
+import getNecessaryData from './parseData.js';
 
 const _parseInt = (x: string) => parseInt(x);
 
 const [startId, endId, compoundId] = process.argv.slice(2).map(_parseInt);
-const API_URL = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/";
+const API_URL = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/';
 
 async function fetchJson(compoundId: number): Promise<RawCompound> {
-	return (await (await fetch(API_URL + compoundId + "/JSON")).json()) as Promise<RawCompound>;
+	return (await (
+		await fetch(API_URL + compoundId + '/JSON')
+	).json()) as Promise<RawCompound>;
 }
 
 async function init() {
