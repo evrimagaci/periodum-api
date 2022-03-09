d6e2d73523597977752278965894e246f5d2fca7
Nesim Tunc <nesimtunc@gmail.com>
2022-03-09 21:28:54 +0900

    Create elements endpoint

--- src/controllers/element.controller.ts
+++ src/controllers/element.controller.ts
@@ -1,23 +1,35 @@
 import { Request, Response } from 'express';
-import log from '../utils/logger';
 import ElementService from '../services/element.service';
-
-class ElementController {
+import BaseController from './base.controller';
+class ElementController extends BaseController {
 	service: ElementService;
 
 	public constructor() {
+		super();
 		this.service = new ElementService();
 	}
 
-	getList = async (_: Request, res: Response) => {
+	getElements = async (_: Request, res: Response) => {
 		try {
-			const elements = await this.service.listAll();
-			res.json({ data: elements });
+			const elements = await this.service.getAll();
+			return this.success(res, elements);
 		} catch (e) {
-			log.error(e);
-			res
-				.status(500)
-				.json({ message: 'Error occured and we have been notified.' });
+			return this.error(res, e as Error);
+		}
+	};
+
+	getElementById = async (req: Request, res: Response) => {
+		try {
+			const elementId = Number(req.params.id);
+			const element = await this.service.getById(elementId);
+
+			if (element === null) {
+				return this.notfound(res, 'Element not found');
+			}
+
+			return this.success(res, element);
+		} catch (e) {
+			return this.error(res, e as Error);
 		}
 	};
 }
--- src/services/element.service.ts
+++ src/services/element.service.ts
@@ -7,8 +7,30 @@ class ElementService {
 		this.prisma = new PrismaClient();
 	}
 
-	listAll = async () => {
-		return this.prisma.elements.findMany();
+	getAll = async () => {
+		return this.prisma.elements.findMany({
+			select: {
+				number: true,
+				name_tr: true,
+				ypos: true,
+				xpos: true,
+				symbol: true,
+				atomic_mass: true,
+				boil_use: true,
+				melt_use: true,
+				block: true,
+				category: true,
+				geochemical_class: true
+			}
+		});
+	};
+
+	getById = async (id: number) => {
+		return this.prisma.elements.findUnique({
+			where: {
+				number: Number(id)
+			}
+		});
 	};
 }
 
