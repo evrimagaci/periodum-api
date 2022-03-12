06d02561dcd19a6e46079e52dc13e984bfe10994
Nesim Tunc <nesimtunc@gmail.com>
2022-03-13 07:53:35 +0900

    Separate getCompound func
    Added a fix note

--- tests/pubchem/api.mock.ts
+++ tests/pubchem/api.mock.ts
@@ -3,7 +3,12 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export const getCompoundById = (): AxiosResponse => {
-	const filePath = path.resolve(`tests/pubchem/fixtures/222.json`);
+	//FIX: If parameter (id: number) set it combines with URL and path.
+	return getCompound(222);
+};
+
+function getCompound(id: number) {
+	const filePath = path.resolve(`tests/pubchem/fixtures/${id}.json`);
 	const data = readFileSync(filePath, 'utf8');
 	return { data: JSON.parse(data) } as AxiosResponse;
-};
+}
