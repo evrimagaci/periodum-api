b0fd3b6ef9a0ac1f77e1dddbdd2d161884082686
T410 <tymnt410@gmail.com>
2022-03-13 02:54:00 +0300

    Seperate service logic. Implement rate limiter

--- src/pubchem/index.ts
+++ src/pubchem/index.ts
@@ -1,52 +1,14 @@
-import fetch from 'node-fetch';
-import { RateLimiter } from 'limiter';
-import { RawCompound } from './types';
-import getNecessaryData from './parseData.js';
+import PubChemService from './service';
 
 const _parseInt = (x: string) => parseInt(x);
 
-const [startId, endId, compoundId] = process.argv.slice(2).map(_parseInt);
-const API_URL = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/';
+const [from, to] = process.argv.slice(2).map(_parseInt);
 
-async function fetchJson(compoundId: number): Promise<RawCompound> {
-	return (await (
-		await fetch(API_URL + compoundId + '/JSON')
-	).json()) as Promise<RawCompound>;
-}
-
-export default async function init(
-	startId: number,
-	endId: number,
-	compoundId: number
-) {
-	const ids: number[] = [];
-
-	for (let i = compoundId; i < compoundId + (endId - startId + 1); i++) {
-		ids.push(i);
-	}
-
-	if (ids.length === 0) {
-		return;
-	}
-
-	const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 250 });
-
-	async function throttleRequest(id: number) {
-		await limiter.removeTokens(1);
-
-		if (id === undefined) {
-			return;
-		}
-		const result = await fetchJson(id);
-		return getNecessaryData(result.Record);
-	}
-
-	ids.forEach(async (id) => {
-		await throttleRequest(id).then((e) => {
-			//eslint-disable-next-line
-			console.log(e?.RecordTitle);
-		});
+export default async function init(from: number, to: number) {
+	const service = new PubChemService();
+	service.getCompounds(from, to).then((res) => {
+		console.log(res);
 	});
 }
 
-init(startId, endId, compoundId);
+init(from, to);
--- src/pubchem/service.ts
+++ src/pubchem/service.ts
@@ -1,12 +1,15 @@
 import PubChemApi from './api';
 import { PubChemCompound, RawCompound } from './types';
 import getNecessaryData from './parseData';
+import { RateLimiter } from 'limiter';
 
 class PubChemService {
 	private api: PubChemApi;
+	private limiter: RateLimiter;
 
 	public constructor() {
 		this.api = new PubChemApi();
+		this.limiter = new RateLimiter({ tokensPerInterval: 1, interval: 250 });
 	}
 
 	async getCompoundById(id: number): Promise<PubChemCompound> {
@@ -15,12 +18,32 @@ class PubChemService {
 		return getNecessaryData(rawData.Record);
 	}
 
+	async throttleRequest(id: number) {
+		await this.limiter.removeTokens(1);
+
+		if (id === undefined) {
+			return;
+		}
+		return this.getCompoundById(id)
+			.then((res) => {
+				return getNecessaryData(res);
+			})
+			.catch((e) => {
+				//eslint-disable-next-line
+				console.error(e);
+			});
+	}
+
 	getCompounds = async (from: number, to: number) => {
-		const result = [];
+		const result: PubChemCompound[] = [];
 		for (let id = from; id <= to; id++) {
-			//TODO: Implement rate limiter
-			const data = await this.getCompoundById(id);
-			result.push(data);
+			await this.throttleRequest(id).then((e) => {
+				if (e) {
+					//eslint-disable-next-line
+					console.log('title', e?.RecordTitle);
+					result.push(e);
+				}
+			});
 		}
 		return result;
 	};
