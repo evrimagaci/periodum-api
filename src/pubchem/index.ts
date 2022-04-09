c5b96efbe5d83d20db837d413d8152609492aafb
Nesim Tunc <nesimtunc@gmail.com>
2022-04-10 00:30:02 +0900

    Create rate limiter

--- src/pubchem/index.ts
+++ src/pubchem/index.ts
@@ -1,36 +1,54 @@
 import PubChemService from './service';
-import { PrismaClient } from '@prisma/client';
-import { notEmpty, parseIntMap } from './utils';
-import { AxiosError } from 'axios';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+// import CompoundService from '../services/compound.service';
+
+import { parseIntMap } from './utils';
+import { RawCompound } from './types';
 
 const [from, to] = process.argv.slice(2).map(parseIntMap);
 
-export default async function init(from: number, to: number) {
-	const service = new PubChemService();
-	const prisma = new PrismaClient();
-	const compounds = service.getCompounds(from, to);
-	const failIds: number[] = [];
-	await compounds
-		.then(async (_res) => {
-			const res = _res.filter(notEmpty);
-			await prisma.compounds.createMany({
-				data: [
-					...res.map((compound) => {
-						return { ...compound };
-					})
-				],
-				skipDuplicates: true
-			});
-		})
-		.catch((error: AxiosError | PrismaClientKnownRequestError) => {
-			if (error && 'config' in error && error.config && error.config.url) {
-				const splitted = error.config.url.split('/');
-				const id = parseInt(splitted[splitted.length - 2]);
+const maxRequestPerInterval = 5;
+const requestInterval = 1000;
+let start = 0;
+let end = 0;
+let timer: NodeJS.Timer;
 
-				failIds.push(id);
-			}
-		});
+export default async function init(from: number, to: number) {
+	if (from <= 0 || from > to) {
+		throw new Error(
+			'Invalid range. Start must be greater than 0 and less than `to`'
+		);
+	}
+
+	start = from;
+	end = to;
+
+	const pubChemService = new PubChemService();
+
+	timer = setInterval(async () => {
+		const requests: Promise<RawCompound>[] = [];
+		const endId = start + maxRequestPerInterval;
+		for (let id = start; id <= endId; id++) {
+			const request = pubChemService.getRawCompoundById(id);
+			requests.push(request);
+		}
+		if (requests.length >= maxRequestPerInterval) {
+			makeRequests(requests);
+		}
+		start = endId;
+		// All completed, stop the timer
+		if (start === end) {
+			clearTimeout(timer);
+		}
+	}, requestInterval);
 }
+
+const makeRequests = async (requests: Promise<RawCompound>[]) => {
+	console.log(requests); // intentional, to be remvoe later.
+	// const compoundService = new CompoundService();
+	//await Promise.all(requests);
+	// TODO: execute all promises at once like above
+	// Import the data using compoundService
+	// Error log case, (later we can create a table)
+};
 
 init(from, to);
