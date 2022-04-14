38bbeb3f2a0b254dfa04b7972353e006b7e44bf7
Doğukan Duran <42766561+goodluckgod@users.noreply.github.com>
2022-04-15 00:06:14 +0300

    Added test script and Github service.

--- src/controllers/github.controller.ts
+++ src/controllers/github.controller.ts
@@ -1,39 +1,21 @@
 import { Request, Response } from 'express';
 import BaseController from './base.controller';
-import { Octokit } from '@octokit/rest';
-import { Endpoints } from '@octokit/types';
+import GithubService from '../services/github.service';
 
 class GithubController extends BaseController {
-	octokit: Octokit;
+	service: GithubService;
 
 	public constructor() {
 		super();
-		this.octokit = new Octokit({
-			auth: process.env.GITHUB_TOKEN,
-			userAgent: 'PeriodumAPI'
-		});
+		this.service = new GithubService();
 	}
 
 	getContributors = async (_: Request, res: Response) => {
 		try {
-			const contributors: Endpoints['GET /users/{username}']['response']['data'][] =
-				[];
-
-			const { data: users }: any =
-				await this.octokit.rest.repos.getContributorsStats({
-					owner: 'evrimagaci',
-					repo: 'periodum'
-				});
-
-			await Promise.all(
-				users.map(async (e: any) => {
-					const { data: user } = await this.octokit.rest.users.getByUsername({
-						username: e.author.login
-					});
-					contributors.push({ user, total: e.total });
-				})
+			const contributors = await this.service.getContributors(
+				'evrimagaci',
+				'periodum'
 			);
-
 			return this.success(res, contributors);
 		} catch (e) {
 			return this.error(res, e as Error);
